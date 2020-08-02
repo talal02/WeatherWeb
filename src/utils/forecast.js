a2ec5b2d28f82fc1596553898c0a43559ddb4bdb
Talal <talalahmed252@gmail.com>
2020-08-02 20:35:04 +0500

    Changes++

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,14 +1,13 @@
 const request = require('request')
-const chalk = require('chalk')
 const forecast = (lat, long, callback) => {
     const url = `https://api.weatherbit.io/v2.0/current?&lat=${lat}&lon=${long}&key=de0f5cff14454e7fa592391cb2978f2f&lang=en`
     request({ url: url, json: true }, (e, { body }) => {
         var { error, data } = body
         data = data[0]
         if(e) {
-            callback(chalk.red.inverse('Unable to connect to the weather API..!'), undefined)
+            callback('Unable to connect to the weather API..!', undefined)
         } else if( error ){
-            callback(chalk.redBright.inverse('Unable to find location..!'), undefined)
+            callback('Unable to find location..!', undefined)
         }else{
             callback(undefined, {
                 temp:   data.temp,
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,16 +1,15 @@
 const request = require('request')
-const chalk = require('chalk')
 const geocoding = (address, callback) => {
     const encodedaddress = encodeURIComponent(address)
     const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodedaddress}.json?access_token=pk.eyJ1IjoidGFsYWwwMiIsImEiOiJja2RhMnJheWYwZHhqMnByeGdxemo0dnhyIn0.xH44JGlA_xTMqZpejWbJIw&limit=1`
     
-    request({ url: url, json: true}, (e, { body }) => {
+    request({ url: url, json: true}, (e, { body } = {}) => {
         var { message, features } = body
         features = features[0]
         if(e) {
-            callback(chalk.red.inverse('Unable to connect to the location API..!'), undefined)
+            callback('Unable to connect to the location API..!', undefined)
         } else if( message || features === undefined ){
-            callback(chalk.redBright.inverse('Location Not Found..! Try Again :)'), undefined)
+            callback('Location Not Found..! Try Again :)', undefined)
         } else {
             callback(undefined, {
                 long:  features.center[0],
